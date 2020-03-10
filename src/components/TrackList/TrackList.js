9355651b2d7dcc81088253752e1bb26923c9d5f9
Margo <margo.bozhenova@gmail.com>
2020-03-11 00:26:31 +0300

    Add initial files and components

---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TrackList.css';
+
+
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {/* <!-- You will add a map method that renders a set of Track components  
+        Tiny Dancer
+Rockabye Baby! | Lullaby Renditions of Elton John--> */}
+      </div>
+    )
+  }
+}
+
+export default TrackList;
