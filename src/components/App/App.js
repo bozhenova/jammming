38152bcf1009803d00877921b6df6bd38e7b1f2f
Margo <margo.bozhenova@gmail.com>
2020-03-12 01:47:25 +0300

    Add addTrack and removeTrack functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,42 @@ import SearchBar from '../SearchBar/SearchBar';
 
 
 
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        { name: 'Numb', artist: 'Linkin park', album: 'Meteora', id: '1' },
+        { name: 'Breaking the Habit', artist: 'Linkin park', album: 'Meteora', id: '2' },
+        { name: 'Don\'t Stay', artist: 'Linkin park', album: 'Meteora', id: '3' },
+        { name: 'From the Inside', artist: 'Linkin park', album: 'Meteora', id: '4' }
+      ],
+      playlistName: 'My Playlist',
+      playlistTracks: [
+        { name: 'Numb', artist: 'Linkin park', album: 'Meteora', id: '5' },
+        { name: 'Breaking the Habit', artist: 'Linkin park', album: 'Meteora', id: '6' },
+        { name: 'Don\'t Stay', artist: 'Linkin park', album: 'Meteora', id: '7' }
+      ],
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id !== track.id)) {
+      tracks.push(track);
+      this.setState({ playlistTracks: tracks });
+    } else {
+      return;
+    }
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(savedTrack => savedTrack.id !== track.id);
+    this.setState({ playlistTracks: tracks });
+  }
 
   render() {
     return (
@@ -17,8 +51,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
-            <Playlist />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './TrackList.css';
+import Track from '../Track/Track';
 
 
 class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {/* <!-- You will add a map method that renders a set of Track components  
-        Tiny Dancer
-Rockabye Baby! | Lullaby Renditions of Elton John--> */}
+        {
+          this.props.tracks.map(track => {
+            return <Track key={track.id} track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />;
+          })
+        }
       </div>
     )
   }
